6032636ac86054cbd1230d79cb92967051ad4e74
Areeba Pervez <areeba.pervez@pithnhire.com>
2024-03-11 18:53:04 +0530

    chat accordion added and chat page added

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,5 +1,11 @@
-export default function Chat(){
-    return(
-        <></>
-    )
+export default function Chat() {
+  return (
+    <>
+      <main>
+      <div className="bg-white p-5 flex-col flex gap-5">
+          <h1 className="text-xl font-semibold">Chats</h1>
+        </div>
+      </main>
+    </>
+  );
 }
--- src/app/notification/page.tsx
+++ src/app/notification/page.tsx
@@ -1,5 +1,11 @@
-export default function Notification(){
-    return(
-        <></>
-    )
+export default function Notification() {
+  return (
+    <>
+      <main>
+        <div className="bg-white p-5 flex-col flex gap-5">
+          <h1 className="text-xl font-semibold">Notifications</h1>
+        </div>
+      </main>
+    </>
+  );
 }
